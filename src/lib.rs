f632afed8d1971866ca994893db854443126ae9a
AbsoluteVirtueXI <absolutevirtuexi@gmail.com>
2022-05-14 02:42:33 +0200

    update import

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
 pub mod camera;
 pub mod debug;
 pub mod player;
+pub mod spritesheets;
 
 pub use camera::CameraPlugin;
 pub use debug::DebugPlugin;
 pub use player::PlayerPlugin;
+pub use spritesheets::SpriteSheetsPlugin;
