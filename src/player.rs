4792c7ceb63bc2d6940adff039f0706a46cd2db7
AbsoluteVirtueXI <absolutevirtuexi@gmail.com>
2022-05-14 02:42:44 +0200

    add direction

--- src/player.rs
+++ src/player.rs
@@ -13,32 +13,45 @@ impl Plugin for PlayerPlugin {
 #[derive(Component, Inspectable)]
 pub(crate) struct Player;
 
+#[derive(Component)]
+enum Direction {
+    Right,
+    Left,
+}
+
 fn spawn_player_system(mut commands: Commands) {
     commands
         .spawn_bundle(SpriteBundle {
+            transform: Transform::from_xyz(-200.0, 0.0, 1.0),
             sprite: Sprite {
                 color: Color::rgb(0.3, 0.5, 0.5),
-                custom_size: Some(Vec2::new(50.0, 50.0)),
+                custom_size: Some(Vec2::new(40.0, 40.0)),
                 ..Default::default()
             },
             ..Default::default()
         })
-        .insert(Player);
+        .insert(Player)
+        .insert(Direction::Right);
 }
 
-fn move_player_system(mut query: Query<&mut Transform, With<Player>>, input: Res<Input<KeyCode>>) {
-    let mut player = query.get_single_mut().unwrap();
+fn move_player_system(
+    mut query: Query<(&mut Transform, &mut Direction), With<Player>>,
+    input: Res<Input<KeyCode>>,
+) {
+    let (mut player_transform, mut player_direction) = query.get_single_mut().unwrap();
     if input.pressed(KeyCode::Right) {
-        player.translation.x += 1.0;
+        *player_direction = Direction::Right;
+        player_transform.translation.x += 1.0;
     }
     if input.pressed(KeyCode::Left) {
-        player.translation.x -= 1.0;
+        *player_direction = Direction::Left;
+        player_transform.translation.x -= 1.0
     }
     if input.pressed(KeyCode::Space) {
-        player.translation.y += 1.0;
+        player_transform.translation.y += 1.0;
     }
     if input.pressed(KeyCode::R) {
-        player.translation.x = 0.0;
-        player.translation.y = 0.0;
+        player_transform.translation.x = 0.0;
+        player_transform.translation.y = 0.0;
     }
 }
